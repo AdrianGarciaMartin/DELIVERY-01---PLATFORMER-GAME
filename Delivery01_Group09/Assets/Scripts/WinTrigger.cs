7b4a7aa28ea4663cb9134d34546ca2d0d8b18fa6
AdrianGarciaMartin <agarciamar@edu.tecnocampus.cat>
2024-01-30 00:11:13 +0100

    Project template

---
+++ Delivery01_Group09/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject WinObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<PlayerMovement>();
+
+        if (player)
+        {
+            ShowWin();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowWin()
+    {
+        WinObject.SetActive(true);
+    }
+}
